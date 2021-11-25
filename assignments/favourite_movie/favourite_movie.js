b1a0d22e37b8105f2c5e184a6987cd6ffcbb72d0
sneha-gowda <snehagowda9899@gmail.com>
2021-11-25 19:30:30 +0530

    Assignment completed

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,15 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    switch(operation){
+        case "add":
+            movies.push(movie);
+            break;
+        case "remove":
+            val=movies.pop();
+            break;
+    }
+    return movies
 }
 
 module.exports = favouriteMovie;
